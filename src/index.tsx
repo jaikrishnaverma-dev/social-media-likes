fb4b56f530cd71882a32b3028627bac259c0b58d
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2023-01-20 13:36:05 +0530

    commits

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+ 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
